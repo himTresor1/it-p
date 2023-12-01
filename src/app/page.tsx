f89006175fed4fe10cb71d53f8c868d85b7e60c4
himTresor1 <amizeropeace@gmail.com>
2023-12-01 15:43:53 +0200

    [feat]:Shapes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,26 +11,38 @@ import Rectangle2 from "@/assets/Rectangle 289.png";
 import Rectangle3 from "@/assets/Rectangle 290.png";
 
 export default function Home() {
+
   return (
     <main className=" ">
-      <div className= "w-full flex min-h-screen flex-row">
-      <div className="w-[40%] h-screen bg-red-600 flex flex-col">
-        <div className="h-[40%]">
-          <Image src={Rectangle2} alt="" />
-          <Image src={Ellipse1} alt="" />
+      <div className=" absolute w-full flex min-h-screen flex-row">
+        <div className="w-[30%] h-screen bg-red-600 flex flex-col">
+          <div className="h-[40%] flex flex-row items-center justify-around">
+            <Image src={Rectangle2} alt="" />
+            <Image src={Ellipse1} alt="" />
+          </div>
+          <div className="h-[20%] flex items-center justify-end">
+            <Image src={line1} alt="" />
+          </div>
+          <div className="h-[40%] flex flex-row items-center justify-around">
+            <Image src={Polygon2} alt="" className="left-4" />
+            <Image src={Rectangle1} alt="" />
+          </div>
         </div>
-        <div className="">
-        <Image src={line1} alt="" />
+        <div className="w-[40%] h-screen bg-yellow-600  flex justify-end">
+          <Image src={Rectangle3} alt="" className="h-[20%] mt-[20%]" />{" "}
         </div>
-        <div className="">
-        <Image src={Rectangle1} alt="" />
-        <Image src={Polygon2} alt="" />
+        <div className="w-[30%] h-screen bg-black">
+          <div className=" ml-10 h-[40%] flex flex-row items-start justify-around ">
+            <Image src={Ellipse2} alt="" />
+            <Image src={rda250} alt=""   className="mt-[20%]" />
+          </div>
+          <div className="h-[20%] flex items-center justify-end">
+            <Image src={line2} alt="" />
+          </div>
+          <div className="h-[40%] flex flex-row items-center justify-around">
+            <Image src={Polygon2} alt="" className="left-4" />
+              </div>
         </div>
-      </div>
-      <div className="w-[20%] h-screen bg-yellow-600">
-        <p>sdf</p>
-      </div>
-      <div className="w-[40%] h-screen bg-black"></div>
       </div>
     </main>
   );
